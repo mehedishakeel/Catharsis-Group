99fda67ee6eceb2c890a9f5919e5559e866fba33
Mehedi Shakeel <mehedishakeelpro@gmail.com>
2022-03-09 23:48:07 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,7 +15,6 @@ sudo timedatectl set-ntp on
 clear
 date
 sleep 3
-clear
 
 
 echo "Installing Requirements Packages"
@@ -30,6 +29,20 @@ do
 done
 clear
 
+
+echo "Setting up UFW Firewall Rules"
+sleep 3
+sudo ufw enable
+Default_ports=(80 443 22)
+for port in ${Default_ports[@]}
+do 
+ sudo ufw allow $port
+done
+clear
+sudo ufw status
+sleep 3
+clear
+
 echo "Setting Up AWS JDK"
 sleep 3
 sudo mkdir files
@@ -41,10 +54,10 @@ clear
 
 echo "Adding Java HOME & PATH"
 java -version
-echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto" >>~/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >>~/.bashrc
 echo "export PATH=$PATH:$JAVA_HOME/bin" >>~/.bashrc
 source ~/.bashrc
-sudo echo "JAVA_HOME SET TO :" $JAVA_HOME
+echo "JAVA_HOME SET TO :" $JAVA_HOME
 sleep 3
 echo "PATH SET TO :" $PATH
 sleep 3
@@ -53,9 +66,9 @@ clear
 echo "Installing Tomcat 8"
 sudo mkdir -p /opt/tomcat
 sudo useradd -m -U -d /opt/tomcat -s /bin/false tomcat
-sudo curl -L https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.76/bin/apache-tomcat-8.5.76.zip --output files/apache-tomcat-8.5.75.zip
+sudo curl -L https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.76/bin/apache-tomcat-8.5.76.zip --output files/apache-tomcat-8.5.76.zip
 sudo unzip files/apache-tomcat-8.5.76.zip
-sudo mv files/apache-tomcat-8.5.76 /opt/tomcat
+sudo mv apache-tomcat-8.5.76 /opt/tomcat
 sudo ln -s /opt/tomcat/apache-tomcat-8.5.76 /opt/tomcat/latest
 sudo chown -R tomcat: /opt/tomcat
 sudo sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
@@ -64,21 +77,6 @@ sudo chmod +x /opt/tomcat/apache-tomcat-8.5.76/bin/startup.sh
 sudo bash /opt/tomcat/apache-tomcat-8.5.76/bin/catalina.sh
 sudo bash /opt/tomcat/apache-tomcat-8.5.76/bin/startup.sh
 clear
-
-echo "Setting up UFW Firewall Rules"
-sleep 3
-sudo ufw enable
-Default_ports=(80 443 8080 22)
-for port in ${Default_ports[@]}
-do 
- sudo ufw allow $port
-done
-clear
-sudo ufw status
-sleep 3
-clear
-
-
 echo "Tomcat Server Started"
 echo "Visit To : http://localhost:8080"
 echo "Thank You!!!"
