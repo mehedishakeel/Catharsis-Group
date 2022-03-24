6cb0bba33a6b71d32d2f4b1704418452d0fd8902
Mehedi Shakeel <mehedishakeelpro@gmail.com>
2022-03-24 14:42:14 +0600

    Update dw.sh

--- dw.sh
+++ dw.sh
@@ -57,7 +57,7 @@ clear
 
 echo "Making Directories & Downloading Resources"
 sudo mkdir -p dw/resources
-cd /dw/resources
+cd dw/resources
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.77/bin/apache-tomcat-8.5.77.tar.gz
 sudo wget https://corretto.aws/downloads/latest/amazon-corretto-8-aarch64-linux-jdk.tar.gz
 sudo wget https://ftp.postgresql.org/pub/source/v10.20/postgresql-10.20.tar.gz
