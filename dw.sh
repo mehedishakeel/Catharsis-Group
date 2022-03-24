bbfebe9d6e615ef0e99c00d2d31f6d65ccdffb34
Mehedi Shakeel <mehedishakeelpro@gmail.com>
2022-03-25 01:44:10 +0600

    Update dw.sh

--- dw.sh
+++ dw.sh
@@ -69,7 +69,7 @@ clear
 
 echo "Extracting Resources "
 cd
-sudo tar -xvzf /dw/resources/apache-tomcat-8.5.77.tar.gz -C dw
+sudo tar -xvzf /dw/resources/apache-tomcat-8.5.77.tar.gz -C dw/tomcat8
 sudo tar -xvzf /dw/resources/postgresql-10.20.tar.gz -C dw
 sudo tar -xvzf /dw/resources/amazon-corretto-8.322.06.2-linux-aarch64.tar.gz -C dw
 echo "Done"
